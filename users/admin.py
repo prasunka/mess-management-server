961f64a47befb6955fb560d035807adbe9bd0dd6
Prasun Kumar <prasun.code@gmail.com>
2021-01-31 11:54:09 +0530

    Added User model and manager classes

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
+
 
 # Register your models here.
+
+User = get_user_model()
+
+admin.site.register(User)
--- users/models.py
+++ users/models.py
@@ -1,6 +1,47 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import PermissionsMixin, BaseUserManager, AbstractBaseUser
 from django.db import models
 
 # Create your models here.
-class User(AbstractUser):
-    pass
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, name, typeAccount, specialRole, password):
+        user = self.model(email=email, name=name, typeAccount=typeAccount, specialRole=specialRole)
+        user.set_password(password)
+        user.is_staff = False
+        user.is_superuser = False
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, name, typeAccount, password, specialRole=None,):
+        user = self.model(email=email, name=name, typeAccount=typeAccount, specialRole=specialRole)
+        user.set_password(password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.is_active = True
+        user.save(using=self._db)
+        return user
+
+    def get_by_natural_key(self, email_):
+        print(email_)
+        return self.get(email=email_)
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True)
+    name = models.CharField(max_length=100)
+    typeAccount = models.CharField(max_length=20)
+    specialRole = models.CharField(max_length=100, null=True)
+    is_staff = models.BooleanField(default=False)
+
+    REQUIRED_FIELDS = ['name', 'typeAccount']
+    USERNAME_FIELD = 'email'
+
+    objects = UserManager()
+
+    def natural_key(self):
+        return self.email
+
+    def get_short_name(self):
+        return self.email
+    
+    def __str__(self):
+        return self.email
