6373b097669f7c0b97d0cf78d90ad588f5d9f00c
Prasun Kumar <prasun.code@gmail.com>
2021-02-03 00:57:16 +0530

    Added serializer and view for user model

--- users/serializers.py
+++ users/serializers.py
@@ -2,10 +2,18 @@ from django.db import transaction
 from rest_framework import serializers
 from dj_rest_auth.registration.serializers import RegisterSerializer
 
+from .models import User
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'name', 'email', 'typeAccount', 'specialRole')
+        read_only_fields = ('id','email')
+
 class CustomRegisterSerializer(RegisterSerializer):
     name = serializers.CharField(max_length=100)
     typeAccount = serializers.CharField(max_length=20)
-    specialRole = serializers.CharField(max_length=100, allow_null=True)
+    specialRole = serializers.CharField(required=False, max_length=100, allow_null=True)
 
     @transaction.atomic
     def save(self, request):
--- users/views.py
+++ users/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import User
+from .serializers import UserSerializer
 
 # Create your views here.
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
