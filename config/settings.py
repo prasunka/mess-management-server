98c10c793716b354a349eeaf1cc01b42c1317aab
Prasun Kumar <prasun.code@gmail.com>
2021-02-03 17:57:09 +0530

    Added static files dirs for local and production

--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 from pathlib import Path
 import environ
+import os
 
 env = environ.Env(
     DEBUG=(bool, False)
@@ -171,6 +172,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),] # For local
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') # For production
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
