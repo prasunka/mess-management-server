921a06fbf08961beaddb55702b679ced010bb3ae
Prasun Kumar <prasun.code@gmail.com>
2021-03-09 19:41:10 +0530

    Corrected ALLOWED_HOSTS

--- config/settings.py
+++ config/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = ['192.168.43.10']
+ALLOWED_HOSTS = ['192.168.43.10', '127.0.0.1', 'localhost']
 
 
 # Application definition
