f2d367298b07fa78c1856bdcc4e3c70d613f60a7
Prasun Kumar <prasun.code@gmail.com>
2021-04-01 00:40:43 +0530

    Fixed specialRole

--- users/models.py
+++ users/models.py
@@ -30,7 +30,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(unique=True)
     name = models.CharField(max_length=100)
     typeAccount = models.CharField(max_length=20)
-    specialRole = models.CharField(max_length=100, null=True)
+    specialRole = models.CharField(max_length=100, null=True, blank=True)
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=False)
 
