68d96039ce085c2b749cd4774c01984c5e123a4b
Prasun Kumar <prasun.code@gmail.com>
2021-04-02 15:49:25 +0530

    Added buyer_name

--- coupons/serializers.py
+++ coupons/serializers.py
@@ -5,6 +5,7 @@ from .models import Coupon
 
 
 class CouponSerializer(serializers.ModelSerializer):
+    buyer_name = serializers.CharField(source='buyer.name')
     class Meta:
         model = Coupon
-        fields = ('uuid', 'buyer', 'is_spent', 'timestamp', 'spent_timestamp')
+        fields = ('uuid', 'buyer', 'buyer_name', 'is_spent', 'timestamp', 'spent_timestamp')
