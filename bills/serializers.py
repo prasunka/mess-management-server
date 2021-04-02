9d0fd2da6004689696bb1cca86677a277d22e510
Prasun Kumar <prasun.code@gmail.com>
2021-04-02 15:25:29 +0530

    Added buyer_name

--- bills/serializers.py
+++ bills/serializers.py
@@ -6,6 +6,7 @@ from .models import Bill
 class BillSerializer(serializers.ModelSerializer):
     month = serializers.ReadOnlyField(source='get_month')
     year = serializers.ReadOnlyField(source='get_year')
+    buyer_name = serializers.CharField(source='buyer.name')
     class Meta:
         model = Bill
-        fields = ('id', 'buyer', 'is_paid', 'bill_from', 'bill_days', 'bill_amount', 'month', 'year')
+        fields = ('id', 'buyer', 'buyer_name', 'is_paid', 'bill_from', 'bill_days', 'bill_amount', 'month', 'year')
